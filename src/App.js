54060980d92e22711cd6fcd5ceec0e0f5c794000
ben-adu <benedictadu@gmail.com>
2022-01-16 16:28:36 -0500

    Child to parent component communication (Bottom-up)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/New Expense/NewExpense";
 
-const App=()=> {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -9,8 +9,7 @@ const App=()=> {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", 
-    title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
       title: "Car Insurance",
@@ -18,12 +17,18 @@ const App=()=> {
       date: new Date(2021, 2, 28),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/New Expense/ExpenseForm.js
+++ src/components/New Expense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -54,10 +54,10 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(expenseDate);
-    setEnteredAmount('');
-    setEnteredDate('');
-    setEnteredTitle('');
+    props.onSaveExpenseData(expenseDate);    
+    setEnteredAmount("");
+    setEnteredDate("");
+    setEnteredTitle("");
   };
 
   return (
